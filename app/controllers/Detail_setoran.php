9644b39cefc5503eaa59554a407bfd6848be6dd8
Gusti Ketut P. Wijaya <32770684+gustikpw@users.noreply.github.com>
2024-07-20 19:26:52 +0800

    Update Detail_setoran.php

--- app/controllers/Detail_setoran.php
+++ app/controllers/Detail_setoran.php
@@ -109,7 +109,7 @@ class Detail_setoran extends CI_Controller
 			$insert = $this->setoran->save($data);
 			// $extend = $this->olt->perpanjangPaketFromDetailSetoran($dt->no_pelanggan, $dt->expired);
 			$update = $this->db->query("UPDATE FROM pelanggan SET expired='$dt->expired' WHERE no_pelanggan='$dt->no_pelanggan'")->affected_rows();
-			return array('status' => TRUE, 'msg' => $extend);
+			return array('status' => TRUE, 'msg' => "");
 		} else {
 			return array('status' => FALSE, 'msg' => '');
 		}
